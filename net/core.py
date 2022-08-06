ae355739c13baf3f807fe15a94332b3727e60a91
yuzh <77485047+ZonghuaYu@users.noreply.github.com>
2022-08-06 22:35:54 +0800

    Add files via upload

---
+++ net/core.py
@@ -0,0 +1,83 @@
+import torch
+
+
+class CoreNet(torch.nn.Module):
+    def __init__(self, Backbone, Depth_hypos, scale,
+                     Homoaggre, Regular, Regress, Refine):
+        """
+
+        @param feature: unit.backbone
+        @param depthhypos: list[unit.depthhypos...]
+        @param scale: unit.scale
+        @param homoaggregate: list[unit.scale, unit.scale, unit.scale]
+        @param regular: list[unit.regualr, unit.regualr, unit.regualr]
+        @param regress: list[unit.regress.depth_regress, unit.regress.confidence_regress]
+        @param refine: unit.refine
+        @param ndepths: [48, 24, 8]
+        """
+        super(CoreNet, self).__init__()
+
+        self.Backbone = Backbone
+        self.Depth_hypos = Depth_hypos
+        self.scale = scale
+        self.Homoaggre = Homoaggre
+        self.Regular = Regular
+        self.Depth_regress, self.Confidence_regress = Regress
+        self.Refine = Refine
+
+        print('{} parameters: {}'.format(self._get_name(), sum([p.data.nelement() for p in self.parameters()])))
+
+    def forward(self, origin_imgs, extrinsics, intrinsics, depth_range):
+        """
+        predict depth
+        @param origin_imgs: （B,VIEW,C,H,W） view0 is ref img
+        @param extrinsics: （B,VIEW,4,4）
+        @param intrinsics: （B,VIEW,3,3）
+        @param depth_range: (B, 2) B*(depth_min, depth_max) dtu: [425.0, 935.0] tanks: [-, -]
+        @return:
+        """
+        origin_imgs = torch.unbind(origin_imgs.float(), 1)  # VIEW*(B,C,H,W)
+
+        # 0. feature extraction
+        features = [self.Backbone(img) for img in origin_imgs] #views * 3 * fea
+
+        depth, depth_hypos, prob_volume, depths = None, None, None, []
+        for stage, (Depth_hypos, Homoaggre, Regular) in \
+                enumerate(zip(self.Depth_hypos, self.Homoaggre, self.Regular)):
+
+            # 1. get features
+            feature = [fea[stage] for fea in features]
+
+            # 2.scale intrinsic matrix & cal proj matrix
+            ref_proj, src_projs = self.scale(intrinsics, extrinsics, stage)
+
+            # 3. depth hypos
+            depth_hypos= Depth_hypos(depth, depth_range, prob_volume, depth_hypos, upsample =True)
+
+            # 4.homo & aggrate
+            cost_volume = Homoaggre(feature, ref_proj, src_projs, depth_hypos)
+
+            # 5.regular
+            prob_volume = Regular(cost_volume)  # (B,D,H,W)
+
+            # 6.depth regress
+            depth = self.Depth_regress(prob_volume, depth_hypos)
+            depths.append(depth)
+
+        # 8. confidence, upsample depth
+        # depth = self.Refine(origin_imgs[0], depth, depth_range)
+        depth = self.Refine(depth, depth_range)
+        depths.append(depth)
+
+        if self.training:
+            return {"depth": depths, }
+
+        confidence = self.Confidence_regress(prob_volume)
+        confidence = torch.nn.functional.interpolate(confidence.unsqueeze(1), size=None, scale_factor=2,
+                                                mode='nearest', align_corners=None).squeeze(1)
+        return {"depth": depth, "confidence": confidence}
+
+
+if __name__=="__main__":
+    pass
+
---
+++ net/unit/base.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from typing import List
+
+
+class ConvBNReLU(nn.Module):
+    def __init__(self,
+                 inchs: int,
+                 outchs: int,
+                 kernel_size: int = 3,
+                 stride: int = 1,
+                 padding: int = 1,
+                 groups: int = 1,
+                 bias: bool = False,
+                 ) -> None:
+        super(ConvBNReLU, self).__init__()
+        self.conv = nn.Conv2d(inchs, outchs, kernel_size, stride, (kernel_size-1)//2, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(outchs)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self,
+                x: torch.Tensor,
+                ) -> torch.Tensor:
+        return self.relu(self.bn(self.conv(x)))
+
+
+class TrConvBNReLU(nn.Module):
+    def __init__(self,
+                 inchs: int,
+                 outchs: int,
+                 kernel_size: int = 3,
+                 stride: int = 1,
+                 padding: int = 1,
+                 output_padding: int=1,
+                 groups: int = 1,
+                 bias: bool = False,
+                 ) -> None:
+        super(TrConvBNReLU, self).__init__()
+        self.conv = nn.ConvTranspose2d(inchs, outchs, kernel_size, stride, padding, output_padding, groups, bias)
+        self.bn = nn.BatchNorm2d(outchs)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self,
+                x: torch.Tensor,
+                ) -> torch.Tensor:
+        return self.relu(self.bn(self.conv(x)))
+
+
+class ConvBNReLU3D(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int=3,
+                 stride=1,
+                 padding: int=1,
+                 groups: int=1,
+                 bias: bool=False,
+                 ) -> None:
+        super(ConvBNReLU3D, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self,
+                x: torch.Tensor
+                ) -> torch.Tensor:
+        return self.relu(self.bn(self.conv(x)))
+
+
+class Res(nn.Module):
+    def __init__(self, chs, ):
+        super(Res, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(chs, chs, 3, 1, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(chs, chs, 3, 1, 1, bias=False),
+        )
+
+    def forward(self,x):
+
+        return x + self.conv(x) * 0.1
+
+
+def homo_warping(src_fea, src_proj, ref_proj, depth_hypos):
+    """
+
+    @param src_fea: [B, C, H, W]
+    @param src_proj: [B, 4, 4]
+    @param ref_proj: [B, 4, 4]
+    @param depth_hypos: [B, Ndepth, 1, 1] or [B,NDepths,H,W]
+    @return: [B, C, Ndepth, H, W]
+    """
+    batch, ndepths, H, W = depth_hypos.shape
+    batch, channels ,height, width= src_fea.shape   #torch.Size([1, 32, 128, 160])
+
+    with torch.no_grad():
+        proj = torch.matmul(src_proj, torch.inverse(ref_proj))
+        rot = proj[:, :3, :3]  # [B,3,3]
+        trans = proj[:, :3, 3:4]  # [B,3,1]
+
+        y, x = torch.meshgrid([torch.arange(0, height, dtype=torch.float32, device=src_fea.device),
+                               torch.arange(0, width, dtype=torch.float32, device=src_fea.device)])
+        y, x = y.contiguous(), x.contiguous()
+        y, x = y.view(height * width), x.view(height * width)
+        xyz = torch.stack((x, y, torch.ones_like(x)))  # [3, H*W]
+        xyz = torch.unsqueeze(xyz, 0).repeat(batch, 1, 1)  # [B, 3, H*W]
+        # del x, y
+
+        rot_xyz = torch.matmul(rot, xyz)  # [B, 3, H*W]
+
+        rot_depth_xyz = rot_xyz.unsqueeze(2).repeat(1, 1, ndepths, 1) * depth_hypos.view(batch, 1, ndepths, H * W) # [B, 3, Ndepth, H*W]
+
+        proj_xyz = rot_depth_xyz + trans.view(batch, 3, 1, 1)  # [B, 3, Ndepth, H*W]
+        proj_xy = proj_xyz[:, :2, :, :] / proj_xyz[:, 2:3, :, :]  # [B, 2, Ndepth, H*W]
+
+        proj_x_normalized = proj_xy[:, 0, :, :] / ((width - 1) / 2) - 1
+        proj_y_normalized = proj_xy[:, 1, :, :] / ((height - 1) / 2) - 1
+        proj_xy = torch.stack((proj_x_normalized, proj_y_normalized), dim=3)  # [B, Ndepth, H*W, 2]
+        # del rot_xyz, rot_depth_xyz, proj_xyz, proj_x_normalized, proj_y_normalized
+
+    warped_src_fea = \
+        F.grid_sample(src_fea, proj_xy.view(batch, ndepths * height, width, 2), mode='bilinear',padding_mode='zeros')
+    warped_src_fea = warped_src_fea.view(batch, channels, ndepths, height, width)
+
+    return warped_src_fea
+
+
+if __name__=="__main__":
+    pass
